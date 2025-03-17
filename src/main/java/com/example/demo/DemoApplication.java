dee143b00de354f4e53a1e64031fc3ceffd146f3
Mahesh Bharat Salunkhe <maheshsalunkhe41999@gmail.com>
2025-03-17 20:30:48 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+// HomeController to handle requests to "/"
+@RestController
+@RequestMapping("/")
+class HomeController {
+
+	@GetMapping
+	public String home() {
+		return "Hello, World!";
+	}
+}
